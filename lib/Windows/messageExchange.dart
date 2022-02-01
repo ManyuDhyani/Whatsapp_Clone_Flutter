bb9e8e439389218e77c6b34320809f354fe74f38
ManyuDhyani <manyudhyani@gmail.com>
2022-02-01 12:34:50 +0530

    Bubble Implementation Chat Details

--- lib/Windows/messageExchange.dart
+++ lib/Windows/messageExchange.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bubble/bubble.dart';
 
 class messageExchange extends StatefulWidget {
   messageExchange({Key? key}) : super(key: key);
@@ -8,10 +9,106 @@ class messageExchange extends StatefulWidget {
 }
 
 class _messageExchangeState extends State<messageExchange> {
+  static const styleSender = BubbleStyle(
+    margin: const BubbleEdges.only(top: 10),
+    alignment: Alignment.topLeft,
+    nip: BubbleNip.leftTop,
+    showNip: false,
+  );
+  static const styleReceiver = BubbleStyle(
+    margin: const BubbleEdges.only(top: 10),
+    alignment: Alignment.topRight,
+    nip: BubbleNip.rightTop,
+    showNip: false,
+    color: const Color.fromRGBO(225, 255, 199, 1),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Message'),
+    return ListView(
+      children: [
+        Bubble(
+          alignment: Alignment.center,
+          color: const Color.fromRGBO(212, 234, 244, 1),
+          child: const Text('TODAY',
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 11)),
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('Hello, World!', textAlign: TextAlign.right),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('How are you?', textAlign: TextAlign.right),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Hi, developer!'),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('Hello, World!', textAlign: TextAlign.right),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('How are you?', textAlign: TextAlign.right),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Hi, developer!'),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('Hello, World!', textAlign: TextAlign.right),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('How are you?', textAlign: TextAlign.right),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Hi, developer!'),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleSender,
+          child: const Text('Well, see for yourself'),
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('Hello, World!', textAlign: TextAlign.right),
+          showNip: true,
+        ),
+        Bubble(
+          style: styleReceiver,
+          child: const Text('How are you?', textAlign: TextAlign.right),
+        ),
+      ],
     );
   }
 }
